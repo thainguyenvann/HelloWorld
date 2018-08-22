5e46ad305455189ae33c666f148abe1095a942cc
unknown <thaitancang@gmail.com>
2018-08-22 15:44:29 +0700

    Sinhvien

---
+++ src/ThaiVan/dev/sinhvien.java
@@ -0,0 +1,19 @@
+package ThaiVan.dev;
+
+public class sinhvien {
+	String name ;
+	int tuoi ;
+	String Masv;
+	
+	
+void thi() {
+
+	System.out.println(name +" lich thi cua sinh vien" );
+ }
+void donghocphi () {
+	
+	System.out.println("Sinh vien da dong hoc phi");
+ }
+}
+
+	
---
+++ src/ThaiVan/dev/sinhvientest.java
@@ -0,0 +1,10 @@
+package ThaiVan.dev;
+
+public class sinhvientest {
+    public static void main(String []agrs) {
+      sinhvien sv = new sinhvien ();
+          sv.name ="Nguyen Van Thai";
+          sv.thi();
+          sv.donghocphi();
+ }
+}
